efe6694be184f360b70401649e4a6ac6f0110857
kipngenohaaron <kipngenohaaron@gmail.com>
2023-06-08 11:43:13 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function introduction(name) {
-    return `Hi, my name is ${name}.`;
+function introduction(kipngeno) {
+    return `Hi, my name is ${kipngeno}.`;
   }
   
-  function introductionWithLanguage(name, language) {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  function introductionWithLanguage(kipngeno, language) {
+    return `Hi, my name is ${kipngeno} and I am learning to program in ${language}.`;
   }
   
-  function introductionWithLanguageOptional(name, language = "JavaScript") {
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  function introductionWithLanguageOptional(kipngeno, language = "JavaScript") {
+    return `Hi, my name is ${kipngeno} and I am learning to program in ${language}.`;
   }
   
